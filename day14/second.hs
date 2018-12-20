5e8f6d8f57efdc0668d7586e0f0944b9461f4115
Alvaro Vilanova Vidal <alvivi@gmail.com>
2018-12-20 09:25:26 +0100

    day 15, part 2

--- day14/second.hs
+++ day14/second.hs
@@ -1,55 +1,31 @@
 #!/usr/bin/env stack
 -- stack script --resolver lts-12.21
-{-# LANGUAGE Strict #-}
 
-import qualified Data.Foldable as Foldable
-import Data.Sequence (Seq, ViewR((:>), EmptyR), (|>))
+{-# LANGUAGE BangPatterns #-}
+
 import qualified Data.Sequence as Seq
+import Data.List (tails, isPrefixOf)
 
-initState :: (Int, Int, Seq Int)
-initState = (0, 1, Seq.fromList [3, 7])
+list :: [Int]
+list =
+  let
+    step !fstIndex !sndIndex !seq =
+      let
+        fstValue = Seq.index seq fstIndex
+        sndValue = Seq.index seq sndIndex
+        sumValue = fstValue + sndValue
+        newValues =
+          if sumValue <= 9
+            then [sumValue]
+            else [div sumValue 10, rem sumValue 10]
+        newSeq = seq <> Seq.fromList newValues
+        newLength = Seq.length seq + length newValues
+        newFstIndex = rem (fstIndex + fstValue + 1) newLength
+        newSndIndex = rem (sndIndex + sndValue + 1) newLength
+      in
+        newValues ++ step newFstIndex newSndIndex newSeq
+  in
+    3 : 7 : step 0 1 (Seq.fromList [3, 7])
 
-appendMany :: [a] -> Seq a -> Seq a
-appendMany [] seq = seq
-appendMany (v:vs) seq = appendMany vs $ seq |> v
-
-step :: (Int, Int, Seq Int) -> (Int, Int, Seq Int)
-step (fstIndex, sndIndex, seq) =
-  let fstValue = Seq.index seq fstIndex
-      sndValue = Seq.index seq sndIndex
-      sumValue = fstValue + sndValue
-      newValues =
-        if sumValue <= 9
-          then [sumValue]
-          else [div sumValue 10, rem sumValue 10]
-      newLength = Seq.length seq + length newValues
-      newFstIndex = rem (fstIndex + fstValue + 1) newLength
-      newSndIndex = rem (sndIndex + sndValue + 1) newLength
-  in (newFstIndex, newSndIndex, appendMany newValues seq)
-
-stepMany :: Int -> (Int, Int, Seq Int) -> (Int, Int, Seq Int)
-stepMany 0 state = state
-stepMany n state = stepMany (n - 1) $ step state
-
-takeRV :: Int -> ViewR a -> [a]
-takeRV 0 _ = []
-takeRV n (xs :> x) = x : takeRV (n - 1) (Seq.viewr xs)
-takeRV n EmptyR = []
-
-takeR :: Int -> Seq a -> [a]
-takeR n seq = reverse $ takeRV n $ Seq.viewr seq
-
-check :: [Int] -> (Int, Int, Seq Int) -> Bool
-check pattern (_, _, seq) = pattern == takeR (length pattern) seq
-
-solveStep :: Int -> [Int] -> (Int, Int, Seq Int) -> Int
-solveStep count pattern state@(_, _, seq) =
-  if check pattern state
-    then Seq.length seq - length pattern
-    else solveStep (count + 1) pattern (step state)
-
-solve :: [Int] -> Int
-solve pattern = (solveStep 0 pattern initState)
-
-main :: IO ()
-main = print $ solve [6, 5, 2, 6, 0, 1]
+find :: [Int] -> Int
+find xs = length $ takeWhile (not . (xs `isPrefixOf`)) $ tails list
